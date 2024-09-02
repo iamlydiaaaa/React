46c51241154015a00cad41a066ce8563e6dfe192
iamldiaaaa <kg0930@naver.com>
2024-09-03 02:18:30 +1200

    ~L.5, useState

--- blog/src/App.js
+++ blog/src/App.js
@@ -1,23 +1,45 @@
+/* eslint-disable */ 
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  let [글제목,글제목변경] = useState(['남자 코트 추천', '강남 우동 맛집', '파이썬독학']);
+  let [따봉, 따봉변경] = useState(0);
+
+  let posts = '강남고기맛집';
+
+  function 제목바꾸기(){
+    var newArray = [...글제목];
+    newArray[0] = '여자 코트 추천';
+    글제목변경( newArray );
+
+    // 글제목변경(['여자 코트 추천', '서초 우동 맛집', '자바독학']);
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="black-nav">
+        <div>개발 blog</div>
+      </div>  
+      <button onClick={() => { 제목바꾸기() } }>변경버튼</button>
+      <div className="list">
+        <h3>{ 글제목[0] } <span onClick={ () => { 따봉변경(따봉 + 1) } }>👍</span>{따봉} </h3>
+        <p>9월 3일 발행</p>
+        <hr/>
+      </div>
+      <div className="list">
+        <h3>{ 글제목[1] }</h3>
+        <p>9월 4일 발행</p>
+        <hr/>
+      </div>
+      <div className="list">
+        <h3>{ 글제목[2] }</h3>
+        <p>9월 5일 발행</p>
+        <hr/>
+      </div>
+
     </div>
   );
 }
