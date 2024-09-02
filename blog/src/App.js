669a2acdb98f258bca902958ffe1b3cab1d60fc7
iamldiaaaa <kg0930@naver.com>
2024-09-03 02:52:33 +1200

    ~L.7, modal

--- blog/src/App.js
+++ blog/src/App.js
@@ -7,15 +7,12 @@ function App() {
 
   let [글제목,글제목변경] = useState(['남자 코트 추천', '강남 우동 맛집', '파이썬독학']);
   let [따봉, 따봉변경] = useState(0);
-
-  let posts = '강남고기맛집';
+  let [modal, setModal] = useState(false);
 
   function 제목바꾸기(){
     var newArray = [...글제목];
     newArray[0] = '여자 코트 추천';
     글제목변경( newArray );
-
-    // 글제목변경(['여자 코트 추천', '서초 우동 맛집', '자바독학']);
   }
 
   return (
@@ -25,7 +22,7 @@ function App() {
       </div>  
       <button onClick={() => { 제목바꾸기() } }>변경버튼</button>
       <div className="list">
-        <h3>{ 글제목[0] } <span onClick={ () => { 따봉변경(따봉 + 1) } }>👍</span>{따봉} </h3>
+        <h3 onClick={ () => { setModal(!modal) }}>{ 글제목[0] } <span onClick={ () => { 따봉변경(따봉 + 1) } }>👍</span>{따봉} </h3>
         <p>9월 3일 발행</p>
         <hr/>
       </div>
@@ -40,8 +37,23 @@ function App() {
         <hr/>
       </div>
 
+      {
+        // 조건식 ? 참일때 실행할코드 : 거짓일때 실행할코드
+        modal == true ? <Modal/> : null
+      }
+
     </div>
   );
+}
+
+function Modal(){
+  return (
+    <div className="modal">
+        <h2>제목</h2>
+        <p>날짜</p>
+        <p>상세내용</p>
+      </div>
+  )
 }
 
 export default App;
